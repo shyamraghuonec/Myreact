02c76f913392a3e8bb94cb96bb9ab1d58f946f36
shyamraghuonec <shyamraghuwanshi9993663832@gmail.com>
2022-01-12 23:10:43 -0800

    Updates

---
+++ precache-manifest.59a6e37bdb5608534522622badfaba5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0afc6737f0f4e31ef98487776412f0c",
+    "url": "/shyam-react/index.html"
+  },
+  {
+    "revision": "f204115fc7027a65b8db",
+    "url": "/shyam-react/static/css/main.94ec8192.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/shyam-react/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/shyam-react/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "f204115fc7027a65b8db",
+    "url": "/shyam-react/static/js/main.b6870387.chunk.js"
+  },
+  {
+    "revision": "4f09dd1654e1d5d4afae",
+    "url": "/shyam-react/static/js/runtime~main.6fe3d1e4.js"
+  }
+]);
